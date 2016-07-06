11b5bb5e5b5142a5fa992abde542ec3ef87db597
jennmn <nappijm@gmail.com>
2016-07-06 18:21:51 -0400

    completed color scheme switcher homework

---
+++ week4/class08/lifetime-supply/js/project.js
@@ -0,0 +1,13 @@
+document.getElementById("click-me").onclick = calculate;
+
+function calculate() {
+	document.getElementById('age').value;
+	document.getElementById('max-age').value;
+	document.getElementById('item').value;
+	document.getElementById('num-per-day').value;
+	document.getElementById('solution')
+max-age
+
+document.getElementById('drink')
+
+}
