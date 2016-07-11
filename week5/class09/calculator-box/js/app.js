1dc93276af2ffe568a5203cb9a7063d9888852f3
jennmn <nappijm@gmail.com>
2016-07-11 18:31:45 -0400

    finished class 9 homework

--- week5/class09/calculator-box/js/app.js
+++ week5/class09/calculator-box/js/app.js
@@ -11,3 +11,63 @@
 // - .click()
 // - . text() or .html()
 // - .css()
+
+$(document).ready(function(){
+
+	var count = 0;
+
+	$('#a10').click(function(){
+		count = count + 10;
+		$('#out').html(count); 
+	});
+
+	$('#a20').click(function(){
+		count = count + 20;
+		$('#out').html(count); 
+	});
+
+	$('#a30').click(function(){
+		count = count + 30;
+		$('#out').html(count); 
+	});
+
+
+	$('#n10').click(function(){
+		count = count - 10;
+		$('#out').html(count); 
+	});
+
+	$('#n20').click(function(){
+		count = count - 20;
+		$('#out').html(count); 
+	});
+
+	$('#n30').click(function(){
+		count = count - 30;
+		$('#out').html(count); 
+	});
+
+
+
+	$('#red').click(function(){
+		$('#out').css('background-color', 'red'); 
+	});
+
+	$('#blue').click(function(){
+		$('#out').css('background-color', 'blue'); 
+	});
+
+
+
+
+
+
+
+	$('#out').click(function(){
+		count = 0;
+		$('#out').html(count);
+		$('#out').css('background-color', 'white');
+	});
+
+
+});
