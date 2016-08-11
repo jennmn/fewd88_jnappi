4962e7a8ea7f3ca7dca13b6a0ba03997a3c929c0
jennmn <nappijm@gmail.com>
2016-08-10 20:11:47 -0400

    moved files

--- _final/js/vineAcademy.js
+++ _final/js/vineAcademy.js
@@ -245,16 +245,3 @@ $('#redTypes').click(function(){
 })
 
 
-
-// $('.open-popup-link').magnificPopup({
-//   type:'inline',
-//   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-// });
-
-$('testbutton').magnificPopup({
-  items: {
-      src: '<div class="white-popup">Dynamically created popup</div>',
-      type: 'inline'
-  },
-  closeBtnInside: true
-});
